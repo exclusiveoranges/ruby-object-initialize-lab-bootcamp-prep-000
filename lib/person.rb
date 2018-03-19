746c8d24673fbabbc7dce06a09b32d71528d2781
Ryan O'Toole <otoolerj@gmail.com>
2018-03-19 18:17:13 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,9 +4,9 @@ class Person
   end
 end
 
-class Dog
-  def initialize(name, breed = "Mutt")
-    @name = name
-    @breed = breed
-  end
-end 
+# class Dog
+#   def initialize(name, breed = "Mutt")
+#     @name = name
+#     @breed = breed
+#   end
+# end
